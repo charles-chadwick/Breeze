d1ed233c0b2031faae5b77022af23b116fb23508
Charles Chadwick <charles@mutuallyassured.com>
2025-06-04 14:15:04 -0700

    Adding Spatie's Activity Log

--- app/Models/Base.php
+++ app/Models/Base.php
@@ -4,13 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Base extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, LogsActivity;
 
     public function __construct(array $attributes = [])
     {
         parent::__construct($attributes);
+    }
+
+    public function getActivitylogOptions() : LogOptions
+    {
+        // TODO: Implement getActivitylogOptions() method.
+        return LogOptions::defaults()
+            ->useLogName('System');
     }
 }
