0d23d206ea8f225d7803ea24e42ee850091dbbec
Charles Chadwick <charles@mutuallyassured.com>
2025-06-05 05:03:29 -0700

    Format

--- tests/Feature/Pages/PatientDiscussionTest.php
+++ tests/Feature/Pages/PatientDiscussionTest.php
@@ -1,18 +1,15 @@
 <?php
 
-use App\Models\Patient;
 use App\Models\Discussion;
+use App\Models\Patient;
 use function Pest\Laravel\get;
 
 it('shows patient discussions', function () {
 
     // Arrange
     $patient = Patient::factory()
-                      ->has(
-                          Discussion::factory())
-                      ->create(
-
-                      );
+                      ->has(Discussion::factory())
+                      ->create();
 
     // Act & Assert
     get(route('patients.discussions', $patient))
