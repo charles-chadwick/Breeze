cd419363b6544eb22eff9d49803c528dbfe45534
Charles Chadwick <charles@mutuallyassured.com>
2025-06-10 20:38:09 -0700

    Added Enums

---
+++ app/Enums/AppointmentType.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+enum AppointmentType: string {
+
+	case HomeVisit   = "Home Visit";
+	case OfficeVisit = "Office Visit";
+	case VideoVisit  = "Video Visit";
+	case PhoneVisit  = "Phone Visit";
+}
---
+++ app/Enums/UserRole.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+enum UserRole: string
+{
+	case SuperAdmin = 'Super Admin';
+	case Staff = 'Staff';
+	case Doctor = 'Doctor';
+	case Nurse = 'Nurse';
+}
