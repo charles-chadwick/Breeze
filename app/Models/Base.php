83c6413f8f70eda210e72ab0a50477c09909b516
Charles Chadwick <charles@mutuallyassured.com>
2025-06-10 19:34:42 -0700

    Added Spatie activity log

---
+++ app/Models/Base.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+class Base extends Model
+{
+    use LogsActivity, SoftDeletes;
+
+    public function __construct($attributes = [])
+    {
+        parent::__construct($attributes);
+
+        $this->fillable = array_merge($this->fillable, [
+            'created_by',
+            'updated_by',
+            'deleted_by',
+        ]);
+    }
+
+    /**
+     * Created by user
+     */
+    public function createdBy() : BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    /**
+     * Updated by user
+     */
+    public function updatedBy() : BelongsTo
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
+
+    /**
+     * Deleted by user
+     */
+    public function deletedBy() : BelongsTo
+    {
+        return $this->belongsTo(User::class, 'deleted_by');
+    }
+
+    /**
+     * Activity Log
+     */
+    public function getActivitylogOptions() : LogOptions
+    {
+        return LogOptions::defaults()
+            ->useLogName("System");
+    }
+}
