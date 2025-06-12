cfcb9463c73fc2fdb3b138e22cb7f37d00064520
Charles Chadwick <charles@mutuallyassured.com>
2025-06-12 07:42:37 -0700

    Working on Prescriptions

---
+++ database/factories/PrescriptionFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Prescription;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/** @extends Factory<Prescription> */
+class PrescriptionFactory extends Factory
+{
+    protected $model = Prescription::class;
+
+    public function definition() : array
+    {
+        return [
+
+
+        ];
+    }
+}
---
+++ database/migrations/2025_06_12_143204_create_prescriptions.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up() : void
+    {
+        Schema::create('prescriptions', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedInteger('patient_id');
+            $table->unsignedInteger('prescriber_id');
+            $table->unsignedInteger('medication_id');
+            $table->timestamp('prescribed_at');
+            $table->string('dosage');
+            $table->string('frequency');
+            $table->string('route');
+            $table->integer('quantity');
+            $table->integer('refills');
+
+            $table->text('instructions')
+                  ->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+            $table->integer('created_by');
+            $table->integer('updated_by')
+                  ->nullable();
+            $table->integer('deleted_by')
+                  ->nullable();
+        });
+    }
+
+    public function down() : void
+    {
+        Schema::dropIfExists('prescriptions');
+    }
+};
