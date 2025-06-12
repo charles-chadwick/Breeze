ff6d7563488e5f6db603560ccc8b265b7febe4c5
Charles Chadwick <charles@mutuallyassured.com>
2025-06-12 07:21:02 -0700

    Removed stuff

--- app/Http/Controllers/AppointmentsController.php
+++ app/Http/Controllers/AppointmentsController.php
@@ -9,14 +9,10 @@ class AppointmentsController extends Controller
 {
     public function index()
     {
-        $appointment_status_options = [];
-        foreach (AppointmentStatus::cases() as $appointment_status) {
-            $appointment_status_options[ $appointment_status->value ] = $appointment_status->name;
-        }
+
         return view('appointments.index', [
             'appointments'               => Appointment::status()
                                                        ->get(),
-            'appointment_status_options' => $appointment_status_options
         ]);
     }
 }
