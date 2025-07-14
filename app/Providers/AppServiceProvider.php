97192b37db5fe308fc337a8b94edf3e46af71a6e
charles <charles@mutuallyassured.com>
2025-07-14 02:12:55 -0700

    Changing logged in user

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (! app()->runningInConsole()) {
-            auth()->loginUsingId(16);
+            auth()->loginUsingId(17);
             CauserResolver::setCauser(auth()->user());
         }
     }
