2da47904c5d57221d3eb5ded4cbc6803c4f61ab2
charles <charles@mutuallyassured.com>
2025-07-13 23:37:07 -0700

    Added a confirm delete modal

---
+++ app/Livewire/ConfirmDelete.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Livewire;
+
+use Flux\Flux;
+use Illuminate\Database\Eloquent\Model;
+use Livewire\Component;
+use Illuminate\View\View;
+
+class ConfirmDelete extends Component
+{
+    public Model  $model;
+    public string $modal_id;
+
+    public function delete() : void
+    {
+        if ($this->model->delete()) {
+            $message = "Successfully deleted record.";
+            $variant = "success";
+        } else {
+            $message = "Error deleting record.";
+            $variant = "error";
+        }
+        Flux::toast($message, heading: ucwords($message), variant: $variant, position: "top-right");
+        Flux::modal($this->modal_id)
+            ->close();
+        $this->dispatch("refresh-page");
+    }
+
+    public function render() : View
+    {
+        return view('livewire.partials.confirm-delete');
+    }
+}
---
+++ resources/views/livewire/partials/confirm-delete.blade.php
@@ -0,0 +1,7 @@
+<form wire:submit="delete">
+    <h2 class="font-bold">Confirm</h2>
+    <div class="text-center">
+        <p>Are you sure you want to delete this?</p>
+        <flux:button type="submit">Yes</flux:button>
+    </div>
+</form>
