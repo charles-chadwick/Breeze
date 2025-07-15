28632d4f402c12c8ab3558561ab3c1dc8ce8f4b2
charles <charles@mutuallyassured.com>
2025-07-14 19:49:05 -0700

    Added custom modal because I don't like Flux's

---
+++ resources/views/components/partials/modal/index.blade.php
@@ -0,0 +1,51 @@
+@props([
+    'id',
+    'title' => null,
+    'initiallyOpen' => false,
+    'buttons' => null,
+    'max_width' => 'max-w-xl'
+])
+
+<div
+    x-data="{ isOpen: @js($initiallyOpen), open() { this.isOpen = true }, close() { this.isOpen = false } }"
+    x-show="isOpen"
+    x-cloak
+    class="fixed inset-0 z-50 flex items-center justify-center bg-black/20 backdrop-opacity-50"
+    x-ref="{{ $id }}"
+    @keydown.escape.window="close()"
+    @open-modal.window="$event.detail.id === '{{ $id }}' && open()"
+    @close-modal.window="$event.detail.id === '{{ $id }}' && close()"
+    x-transition.opacity
+>
+    <div
+        class="absolute inset-0"
+        @click="close()"
+    ></div>
+
+    <div
+        @click.stop
+        class="bg-white backdrop-opacity-100 rounded-lg shadow-xl {{ $max_width }} w-full mx-4 p-6 z-10 transform transition-all"
+        x-show="isOpen"
+        x-transition
+    >
+        @if($title)
+            <h2 class="text-xl font-semibold mb-4">{{ $title }}</h2>
+        @endif
+
+        <div {{ $attributes }}>
+        {{ $slot }}
+        </div>
+
+        <div class="mt-6 text-center">
+            @if($buttons != null)
+                {{ $buttons }}
+            <button
+                @click="close()"
+                class="px-4 py-2  text-zinc-900 rounded"
+            >
+                Close
+            </button>
+            @endif
+        </div>
+    </div>
+</div>
---
+++ resources/views/components/partials/modal/trigger.blade.php
@@ -0,0 +1,6 @@
+<!-- Trigger buttons -->
+
+<a x-data @click="$dispatch('open-modal', { id: '{{ $id }}' })" href="#">{{ $slot }}</a>
+
+
+
